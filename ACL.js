1026a409fa1c4209f36b817ff7a6efa47bff040b
Nikhil Gayakwad <118470700+nikhilgayakwad121@users.noreply.github.com>
2026-08-16 22:52:11 +0530

    Delete ACL.js

--- ACL.js
+++
@@ -1,9 +0,0 @@
--->QUESTION: 1
-Operation to Perform: Read Permission
-First ACL created: incident.none (role required itil and itil_admin)
-Second ACL created: incident.* (role required itil_admin)
-Result: only user with itil_admin can have read access incident.* is only providing itil_admin role a read access. (This happens because both has itil_admin common)
-but if:
-First ACL created: incident.none (role required itil_admin)
-Second ACL created: incident.* (role required itil)
-Result: User with itil role only will not view any record because he only have read access at the field level and not on the record/row level.
